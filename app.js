eff6275afbed1797563219ea3f6a23fe07f75fd8
AvarageCodeEnjoyer <111468739+AvarageCodeEnjoyer@users.noreply.github.com>
2023-03-08 18:29:00 -0700

    added comments

--- app.js
+++ app.js
@@ -1,22 +1,75 @@
 const form = document.querySelector('form')
 const input = document.getElementById('input')
+const choose = document.getElementsByName('choose')
+const loop = document.getElementById('loop')
+const numbers = document.getElementById('numbers')
+let squares, primes
 
 form.addEventListener('submit', e => {
   e.preventDefault()
-  isPrime(input.value)
+  console.clear()
+  console.time('Count')
+
+  let inputVal = input.value
+  
+  // Check for primes
+  if (choose[0].checked){
+    if (loop.checked){
+      for (let i = 2; i <= inputVal; i++){
+        isPrime(i)
+      }
+      return
+    }
+    isPrime(inputVal)
+  }
+
+  // Check for squares
+  if (choose[1].checked){
+    if (loop.checked){
+      for (let i = 2; i <= inputVal; i++){
+        isSquare(i)
+      }
+      return
+    }
+    isSquare(inputVal)
+  }
+  console.timeEnd('Count')
 })
 
+/* -------------------------------------------------------------------------- */
+/*                      Functions to check for property's                     */
+/* -------------------------------------------------------------------------- */
+
+// Check if number is Prime
 function isPrime(num) {
-  if (num <= 1) return
-  if (num % Math.sqrt(num) === 0) {
-    console.log(`${num} is a square`)
+  let primes = []
+  if (num <= 1) {
+    console.log('Number must be greater then 1')
     return
   }
   for (let i = 2; i <= Math.sqrt(num); i++) {
     if (num % i === 0) {
-      console.log(`${num} is NOT prime`)
+      console.log(`${num} is NOT a prime`)
       return false
     }
   }
   console.log(`${num} is prime`)
+  primes.push(num)
+  return true
+}
+
+// Check if number is a Square
+function isSquare(num, squares){
+  squares = []
+  if (num <= 1) {
+    console.log('Number must be greater then 1')
+    return false
+  }
+  if (num % Math.sqrt(num) === 0) {
+    console.log(`${num}, is ${Math.sqrt(num)} squared`)
+    squares.push(num)
+    return true
+  }
+  console.log(`${num} is NOT a square`)
+  return false
 }
